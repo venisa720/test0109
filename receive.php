4484be443ab3013b2770adcda8b5fa404ad85793
venisa720 <35246276+venisa720@users.noreply.github.com>
2018-01-09 11:54:42 +0800

    Update receive.php

--- receive.php
+++ receive.php
@@ -1,9 +1,9 @@
-<?php
+ 	<?php	
  	$json_str = file_get_contents('php://input'); //接收request的body	
  	$json_obj = json_decode($json_str);//轉JSON格式	
  	 	
  	$myfile = fopen("log.txt","w+") or die("Unable to open file");	
  	fwrite($myfile, "\xEF\xBB\xBF".$json_str);	
  	fclose($myfile)	
-
-?>
+ 	 	
+ 	?>	
