6388f404e960c474f9659578159a0b552322cadf
venisa720 <35246276+venisa720@users.noreply.github.com>
2018-01-09 11:36:47 +0800

    get line message

--- receive.php
+++ receive.php
@@ -1,3 +1,9 @@
 <?php
-  echo "hello i'm venisa"
+  $json_str = file_get_contents('php://input'); //接收request的boby
+  $json_obj = json_decode($json_str);  //轉json格式
+
+  $myfile = fopen("log.txt","w+") or die("unable to open file!");  //設定一個Log.txt 用來印訊息
+  fwrite($myfile, "\xEF\xBB\xBF".$json_str);  //在字串前加入\xEF\xBB\xBF轉成utf8格式
+  fclose($myfile);
+
 ?>
